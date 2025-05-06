a23f008308d290c313e589c24d4690efc7b73e6c
Sinclair Fuh <sfuh1@umbc.edu>
2025-05-06 01:38:52 -0400

    Added filterable word list. TODO: polish the CSS of everything to add user feedback to the site.

--- src/app/word_setter.tsx
+++ src/app/word_setter.tsx
@@ -1,14 +1,43 @@
 import styles from './css/word_setter.module.css';
 import { FixedSizeList as List } from 'react-window';
+import { useState } from 'react';
 
-export default function Word_setter({wordData}: any){
+
+
+export default function Word_setter({wordData, secretSetter}){
+
+    const handleClick = (event) => {
+        const word = event.target.innerHTML;
+        secretSetter(word.trim());
+    };
+
+    const handleFilter = (event) => {
+        setFilter(event.target.value.toUpperCase());
+        setWordEntries(originalWordData.filter(item => item.includes(filter)));
+    };
+
+    let [filter, setFilter] = useState("");
+    let [listLen, setListLen] = useState(wordData.length);
+    const originalWordData = wordData.map( (item, _) => item.replace('\r', '').toUpperCase());
+    let [wordEntries, setWordEntries] = useState(originalWordData);
+
+    //NOTE: to work around react-window's current limitations, I had to use a callback that doesn't take in any parameters.
+    //As a result, I need to access the selected word by reading in the inner HTML of the row element.
+
+    //UNUSED CSS FILTERING
+    //{display: ({filter}.length == 0)?'inline':(wordData[index].includes({filter}))?'inline':'none'}
+    //(filter.length==0)?styles.listItem:(wordData[index].replace('\r', '').toUpperCase().includes(filter.toUpperCase()))?styles.listItem:styles.hidden
     const row = ({index, style}) => (
-        <div className = {styles.listItem} style={style}> {wordData[index]} </div>
+        <div className={styles.listItem} style={style} onClick={handleClick}>{wordEntries[index]}</div>
     );
 
     return(
-      <List height={150} itemCount = {wordData.length} itemSize={35} width={300}>
-        {row}
-      </List>
+        <div>
+            <List height={150} itemCount = {wordEntries.length} itemSize={35} width={300}>
+                {row}
+            </List>
+            <input onChange={handleFilter}></input>
+            <div>{filter}</div>
+        </div>
     );
 }
--- src/app/wordle_box.tsx
+++ src/app/wordle_box.tsx
@@ -3,6 +3,7 @@ import styles from './css/wordle_box.module.css';
 import Wordle_row from './wordle_row.tsx';
 import Word_setter from './word_setter.tsx';
 import { useState } from 'react';
+import { FixedSizeList as List } from 'react-window';
 
 interface Props{
     wrows: string[];
@@ -21,15 +22,15 @@ export default function Wordle_box({wordData}: any){
     let wrows = [wrow1, wrow2, wrow3, wrow4, wrow5, wrow6];
 
     let [secret, setSecret] = useState("OPENS");
-    const setSecret_child = (secret) => {
-        setSecret(secret);
-    };
     const url = "https://d6efzmgcn7.execute-api.us-east-2.amazonaws.com/test-1_5/";
 
     async function getWord(){
         try{
             const response = await fetch(url, {method: "POST", body: JSON.stringify({secret: secret})});
+            console.log(JSON.stringify({secret: secret}));
+            console.log(response);
             const body = await response.json();
+            console.log(body);
             const result = body['result'];
             console.log(result);
 
@@ -67,7 +68,8 @@ export default function Wordle_box({wordData}: any){
          <div className={styles.box}>
             {wrows.map((_, i) => <Wordle_row key={i} data={wrows[i]}></Wordle_row>)}
             <button onClick={getWord}>Getword</button>
-            <Word_setter wordData={wordData}></Word_setter>
+            <Word_setter wordData={wordData} secretSetter={setSecret}></Word_setter>
+            <div> {secret} </div>
          </div>
     );
 }
